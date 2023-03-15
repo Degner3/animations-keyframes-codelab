44580939bd5db632980c3f2b92b71b9ff81e7ed6
Degner3 <niklaschristiansen@live.dk>
2023-03-15 12:21:01 +0100

    asd

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -3,7 +3,10 @@
 let myBall=document.getElementById('ballOne');
 let myBuhButton=document.getElementById('buhOneButton');
 
+myBuhButton.addEventListener('click', () => {
 
+    myBall.classList.toggle('animateRight');
+});
 
 
 /*opgave 2*/
@@ -11,12 +14,36 @@ let myBuhButton=document.getElementById('buhOneButton');
 let myBalls=document.getElementsByClassName('balls');
 let myBuhTwoButton=document.getElementById('buhTwoButton');
 
+myBuhTwoButton.addEventListener('click', () => {
 
+    for (const myBall of myBalls) {
+        myBall.classList.toggle('animateRight')
+    }
+});
 
 /*opgave 3*/
 
 
+let myhardballs = document.getElementsByClassName('hardBalls');
+let myBuhThreeButton = document.getElementById('buhThreeButton');
 
+myBuhThreeButton.addEventListener('click', () => {
+
+    let myDiraction = "Right";
+    for (let myBall of myhardballs) {
+
+        if (myDiraction == "Right") {
+            myBall.classList.toggle('loopRight');
+            myDiraction = "Left";
+        }
+        else {
+            myBall.classList.toggle('loopLeft');
+            myDiraction = "Right";
+        }
+
+
+    }
+});
 
 
 
